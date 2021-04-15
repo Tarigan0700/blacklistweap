812a63264f0c5a34fdab5524206f9b76a78c2add
DO <72868704+Tarigan0700@users.noreply.github.com>
2021-04-16 03:03:10 +0700

    Create client.lua

---
+++ client.lua
@@ -0,0 +1,27 @@
+CreateThread(function()
+    local sleep = 100
+	while true do
+        Wait(sleep)
+        local playerPed = PlayerPedId()
+        local nothing, weapon = GetCurrentPedWeapon(playerPed, true)
+        local blacklisted, name = isWeaponBlacklisted(weapon)
+		if blacklisted then
+			RemoveWeaponFromPed(playerPed, weapon)
+            if Config.BlacklistWeaponLog then
+                TriggerServerEvent('blacklist:dclog', 'Yasaklı Silah Alındı : '..name)
+            end
+            if Config.KickPlayer then
+                TriggerServerEvent('blacklist:drop')
+            end
+		end
+	end
+end)
+
+function isWeaponBlacklisted(model)
+	for _, blacklistedWeapon in pairs(Config.BlacklistedWeapons) do
+		if model == GetHashKey(blacklistedWeapon) then
+			return true, blacklistedWeapon
+		end
+	end
+	return false, nil
+end
